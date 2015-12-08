697a0bdce66d7e312844df80224f34f9c4633df9
Cristina Radulescu <cristinaradulescu@192-168-0-100.rdsnet.ro>
2015-12-08 22:23:11 +0200

    Initial Commit

---
+++ FindMyFriends/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FindMyFriends
+//
+//  Created by Cristina Radulescu on 12/8/15.
+//  Copyright © 2015 Cristina Radulescu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
